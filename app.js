d0c3ee668e09af256cc3a07a4a57708ef2efcc72
Cherno Badjan <badjancherno@gmail.com>
2021-01-13 14:23:25 -0800

    HTML Layput

---
+++ app.js
@@ -0,0 +1,7 @@
+// import functions and grab DOM elements
+const playButton = document.getElementById('play-button');
+const winDisplay = document.getElementById('number-of-wins';)
+
+// initialize state
+
+// set event listeners to update state and DOM
---
+++ get-random-throw.js
@@ -0,0 +1,4 @@
+function getRandomThrow() {
+    Math.floor(Math.random() * 3) + 1;  // returns random integer from 1-3
+    console.log(getRandomThrow)
+}
