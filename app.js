2e8cc565b035f01cf4d8b0cc504f7cb3facca2a7
Cherno Badjan <badjancherno@gmail.com>
2021-01-13 14:25:17 -0800

    1st dev commit

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // import functions and grab DOM elements
 const playButton = document.getElementById('play-button');
-const winDisplay = document.getElementById('number-of-wins';)
+const winDisplay = document.getElementById('number-of-wins');
+const lossDisplay = document.getElementById('number-of-losses');
+const totalDisplay = document.getElementById('total')
 
 // initialize state
+let wins = 0;
+let losses = 0;
+let draw = 0;
+let total = 0;
 
 // set event listeners to update state and DOM
