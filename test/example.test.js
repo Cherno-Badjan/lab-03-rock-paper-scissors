f6215691cd44a988de3a5995d470b5cb90f4ec66
Cherno Badjan <badjancherno@gmail.com>
2021-01-13 22:07:41 -0800

    testing getRandomThrow

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,42 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { getRandomThrow } from '../get-random-throw.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('return rock if rockPaperScissors is 1', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
+    const expected = 'Rock';
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = getRandomThrow(1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('return paper if rockPaperScissors is 2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'Paper';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getRandomThrow(2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('return paper if rockPaperScissors is 2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'Rock';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getRandomThrow(2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
